27dcb7cf69d22f63750307ccb96512050020b5e2
Yuerino <50553280+Yuerino@users.noreply.github.com>
2023-09-12 20:20:08 +0200

    remove hard coded path

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,14 @@
-#include <iostream>
-#include <stdexcept>
-
-#include <vulkan/vulkan.hpp>
-
 #include "core/App.hpp"
-#include "core/GlfwApi.hpp"
 #include "core/util.hpp"
 #include "scene/BasicScene.hpp"
 
 int main() {
-#ifdef __linux__
-    nae::util::fragShaderPath = "/home/yuerino/project/not-an-engine/shader/shader.frag.spv";
-    nae::util::vertShaderPath = "/home/yuerino/project/not-an-engine/shader/shader.vert.spv";
-#elif _WIN32
-    nae::util::fragShaderPath = "C:/Users/yueri/Documents/Project/not-an-engine/shader/shader.frag.spv";
-    nae::util::vertShaderPath = "C:/Users/yueri/Documents/Project/not-an-engine/shader/shader.vert.spv";
-#endif
+    nae::util::fragShaderPath = "../shader/shader.frag.spv";
+    nae::util::vertShaderPath = "../shader/shader.vert.spv";
 
-    try {
-        nae::App app{};
-        app.setActiveScene<nae::BasicScene>();
-        app.run();
-    } catch (const nae::GlfwException &ex) {
-        std::cerr << "GLFW error code: " << ex.getErrorCode() << ", description: " << ex.what() << '\n';
-        return EXIT_FAILURE;
-    } catch (const vk::Error &ex) {
-        std::cerr << "Vulkan error: " << ex.what() << '\n';
-        return EXIT_FAILURE;
-    } catch (const std::exception &ex) {
-        std::cerr << ex.what() << '\n';
-        return EXIT_FAILURE;
-    } catch (...) {
-        std::cerr << "Unknown exception" << '\n';
-        return EXIT_FAILURE;
-    }
+    nae::App app{};
+    app.setActiveScene<nae::BasicScene>();
+    app.run();
 
     return EXIT_SUCCESS;
 }
--- src/scene/BasicScene.cpp
+++ src/scene/BasicScene.cpp
@@ -20,35 +20,29 @@ void BasicScene::onAttach() {
 
     //! Plane texture
     auto pPlaneTexture = std::make_unique<Texture>(vk::Extent2D{1024, 1024});
-    pPlaneTexture->setTexels("C:/Users/yueri/Documents/Project/not-an-engine/model/PavingStones135_1K.png");
+    pPlaneTexture->setTexels("../model/PavingStones135_1K.png");
 
     //! Plane
     auto *pPlaneEntity = getEntityManager().addEntity(std::make_unique<Entity>("plane"));
-    pPlaneEntity->addComponent<Mesh>(
-            std::make_unique<Model>("C:/Users/yueri/Documents/Project/not-an-engine/model/floor.obj"),
-            std::move(pPlaneTexture));
+    pPlaneEntity->addComponent<Mesh>(std::make_unique<Model>("../model/floor.obj"), std::move(pPlaneTexture));
     pPlaneEntity->addComponent<Transform>(glm::vec3{0.0f}, glm::vec3{0.0f}, glm::vec3{10.0f, 10.0f, 10.0f});
 
     //! 42 obj texture
     auto p42Texture = std::make_unique<Texture>(vk::Extent2D{1024, 1024});
-    p42Texture->setTexels("C:/Users/yueri/Documents/Project/not-an-engine/model/Concrete042A_1K.png");
+    p42Texture->setTexels("../model/Concrete042A_1K.png");
 
     //! 42 obj
     auto *p42Entity = getEntityManager().addEntity(std::make_unique<Entity>("42"));
-    p42Entity->addComponent<Mesh>(
-            std::make_unique<Model>("C:/Users/yueri/Documents/Project/not-an-engine/model/forty_two.obj"),
-            std::move(p42Texture));
+    p42Entity->addComponent<Mesh>(std::make_unique<Model>("../model/forty_two.obj"), std::move(p42Texture));
     p42Entity->addComponent<Transform>(glm::vec3{5.0f, 0.0f, 0.0f});
 
     //! Vase obj texture
     auto pVaseTexture = std::make_unique<Texture>(vk::Extent2D{1024, 1024});
-    pVaseTexture->setTexels("C:/Users/yueri/Documents/Project/not-an-engine/model/Wood054_1K.png");
+    pVaseTexture->setTexels("../model/Wood054_1K.png");
 
     //! Vase obj
     auto *pVaseEntity = getEntityManager().addEntity(std::make_unique<Entity>("vase"));
-    pVaseEntity->addComponent<Mesh>(
-            std::make_unique<Model>("C:/Users/yueri/Documents/Project/not-an-engine/model/vase.obj"),
-            std::move(pVaseTexture));
+    pVaseEntity->addComponent<Mesh>(std::make_unique<Model>("../model/vase.obj"), std::move(pVaseTexture));
     pVaseEntity->addComponent<Transform>(glm::vec3{0.0f}, glm::vec3{0.0f}, glm::vec3{5.0f, 5.0f, 5.0f});
 }
 
